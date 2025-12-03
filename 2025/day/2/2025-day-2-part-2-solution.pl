798879d4c693c4c7ad7904492adb1bc56560c308
Mike Thelen <boshvark@gmail.com>
2025-12-03 14:10:07 -0500

    Simplify the regex, no need to manually set max repeats.

--- 2025/day/2/2025-day-2-part-2-solution.pl
+++ 2025/day/2/2025-day-2-part-2-solution.pl
@@ -12,12 +12,11 @@ for (<>) {
     next unless $range =~ /^(\d+)-(\d+)$/;
     my ($low, $high) = ($1, $2);
     next unless $high > $low;
-    my $max_repeat = length $high;
 
     # Check every ID for repeated digits. A sequence must repeat at least once,
     # and up to N times where N is the number of digits at the top of the range.
     for my $id ($low .. $high) {
-      $total = $total + $id if $id =~ /^(.*)\1{1,$max_repeat}$/;
+      $total = $total + $id if $id =~ /^(.*)\1+$/;
     }
   }
 }
